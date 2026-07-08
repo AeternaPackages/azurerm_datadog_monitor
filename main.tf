cea009e6ed5b6d7de354f230239c698c4fd2cbcf
Daniel Allen <dan@aeternagroup.net>
2026-07-08 23:38:34 +0100

    Update to schema version v4.80.0

---
+++ main.tf
@@ -0,0 +1,39 @@
+locals {
+  datadog_monitors = { for k1, v1 in var.datadog_monitors : k1 => { datadog_organization = v1.datadog_organization, identity = v1.identity, location = v1.location, monitoring_enabled = v1.monitoring_enabled, name = v1.name, resource_group_name = v1.resource_group_name, sku_name = v1.sku_name, tags = v1.tags, user = v1.user } }
+
+  datadog_monitor_sso_configurations = merge([
+    for k1, v1 in var.datadog_monitors : {
+      for k2, v2 in coalesce(v1.datadog_monitor_sso_configurations, {}) :
+      "${k1}/${k2}" => merge(v2, {
+        datadog_monitor_id = module.datadog_monitors.datadog_monitors["${k1}"].id
+      })
+    }
+  ]...)
+
+  datadog_monitor_tag_rules = merge([
+    for k1, v1 in var.datadog_monitors : {
+      for k2, v2 in coalesce(v1.datadog_monitor_tag_rules, {}) :
+      "${k1}/${k2}" => merge(v2, {
+        datadog_monitor_id = module.datadog_monitors.datadog_monitors["${k1}"].id
+      })
+    }
+  ]...)
+}
+
+module "datadog_monitors" {
+  source           = "git::https://github.com/AeternaModules/azurerm_datadog_monitor.git?ref=v4.80.0"
+  datadog_monitors = local.datadog_monitors
+}
+
+module "datadog_monitor_sso_configurations" {
+  source                             = "git::https://github.com/AeternaModules/azurerm_datadog_monitor_sso_configuration.git?ref=v4.80.0"
+  datadog_monitor_sso_configurations = local.datadog_monitor_sso_configurations
+  depends_on                         = [module.datadog_monitors]
+}
+
+module "datadog_monitor_tag_rules" {
+  source                    = "git::https://github.com/AeternaModules/azurerm_datadog_monitor_tag_rule.git?ref=v4.80.0"
+  datadog_monitor_tag_rules = local.datadog_monitor_tag_rules
+  depends_on                = [module.datadog_monitors]
+}
+
---
+++ variables.tf
@@ -0,0 +1,88 @@
+variable "datadog_monitors" {
+  description = <<EOT
+Map of datadog_monitors, attributes below
+Required:
+    - location
+    - name
+    - resource_group_name
+    - sku_name
+    - datadog_organization (block)
+    - user (block)
+Optional:
+    - monitoring_enabled
+    - tags
+    - identity (block)
+Nested datadog_monitor_sso_configurations (azurerm_datadog_monitor_sso_configuration):
+    Required:
+        - enterprise_application_id
+    Optional:
+        - name
+        - single_sign_on
+        - single_sign_on_enabled
+Nested datadog_monitor_tag_rules (azurerm_datadog_monitor_tag_rule):
+    Optional:
+        - name
+        - log (block)
+        - metric (block)
+EOT
+
+  type = map(object({
+    location            = string
+    name                = string
+    resource_group_name = string
+    sku_name            = string
+    monitoring_enabled  = optional(bool) # Default: true
+    tags                = optional(map(string))
+    datadog_organization = object({
+      api_key           = string
+      application_key   = string
+      enterprise_app_id = optional(string)
+      linking_auth_code = optional(string)
+      linking_client_id = optional(string)
+      redirect_uri      = optional(string)
+    })
+    user = object({
+      email        = string
+      name         = string
+      phone_number = optional(string)
+    })
+    identity = optional(object({
+      type = string
+    }))
+    datadog_monitor_sso_configurations = optional(map(object({
+      enterprise_application_id = string
+      name                      = optional(string) # Default: "default"
+      single_sign_on            = optional(string)
+      single_sign_on_enabled    = optional(string)
+    })))
+    datadog_monitor_tag_rules = optional(map(object({
+      name = optional(string) # Default: "default"
+      log = optional(object({
+        aad_log_enabled = optional(bool)
+        filter = optional(object({
+          action = string
+          name   = string
+          value  = string
+        }))
+        resource_log_enabled     = optional(bool)
+        subscription_log_enabled = optional(bool)
+      }))
+      metric = optional(object({
+        filter = optional(object({
+          action = string
+          name   = string
+          value  = string
+        }))
+      }))
+    })))
+  }))
+
+  validation {
+    condition = alltrue(concat(
+      [for kk in keys(var.datadog_monitors) : !strcontains(kk, "/")],
+      flatten([for k0, v0 in var.datadog_monitors : [for kk in keys(coalesce(v0.datadog_monitor_sso_configurations, {})) : !strcontains(kk, "/")]]),
+      flatten([for k0, v0 in var.datadog_monitors : [for kk in keys(coalesce(v0.datadog_monitor_tag_rules, {})) : !strcontains(kk, "/")]])
+    ))
+    error_message = "Map keys in this package must not contain '/': it is used internally as a nesting-key separator, so a key containing it can silently collide two different nested entries into one. Rename the offending key(s)."
+  }
+}
